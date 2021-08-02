b9d995a8c5dc9f18fbfafc2cb39b1ebcd2a14c35
Alexey <traf10@bk.ru>
2021-08-02 13:44:08 +0700

    j_2_hw_4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    public TextArea textArea;
+    @FXML
+    public TextField textField;
+
+
+    @FXML
+    public void sendMsg(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+
+    }
 }
